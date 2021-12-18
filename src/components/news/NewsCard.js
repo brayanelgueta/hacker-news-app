04b9c2cee07c48d64db21a82f4805c45719e1f91
Brayan Elgueta <brayanelguetaantiman@gmail.com>
2021-12-18 17:27:11 -0300

    first api test and props components added

--- src/components/news/NewsCard.js
+++ src/components/news/NewsCard.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './NewsCard.css'
 
-export default function NewsCard() {
+export default function NewsCard(props) {
     return (
         
             <div className="card-content">
                 <div className="card-body">
                     <div className="timestamp">
                         <img src={process.env.PUBLIC_URL + "./img/iconmonstr-time-2.png"} alt="time-icon"></img>
-                        <p className="m-0">3 hours ago by author</p>
+                        <p className="m-0">3 hours ago by {props.author}</p>
                     </div>
                     <div className="description">
-                        <p className="m-0">Yes, React is taking over front-end development. The question is why.</p>
+                        <p className="m-0">{props.title}</p>
                     </div>
                 </div>
                 <div className="fav-container">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,20 +8,37 @@ import axios from 'axios';
 
 export default function Home() {
 
-  
+    const [posts, setPosts] = useState([])
 
+
+    useEffect(() => {
+        axios.get('https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0')
+          .then(response => {
+            const res = response.data
+            setPosts(res.hits)
+          })
+    }, [])
+console.log(posts)
     return (
         <div className="App">
       
         
       <div className="news-card-container">
+        {
+          posts.map((item, index) => {
+            return (
+              <NewsCard 
+                author={item.author}
+                title={item.story_title}
+              />
+            )
+            
+          })
 
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
+
+        }
+        
+       
       </div>
       <div className="pagination">
         paginacion
